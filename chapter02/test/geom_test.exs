f96249ea87cb5c5ac97049bc04800a5d1e43ebc9
Jingjing Duan <jduan@twitter.com>
2014-09-11 12:57:38 -0700

    Etude 2-2

--- chapter02/test/geom_test.exs
+++ chapter02/test/geom_test.exs
@@ -1,8 +1,15 @@
 defmodule GeomTest do
   use ExUnit.Case
 
-  test "area" do
+  test "Etude 2-1 area" do
     assert Geom.area(3, 4) == 12
     assert Geom.area(12, 7) == 84
   end
+
+  test "Etude 2-2 area with default values" do
+    assert Geom.area(7, 5) == 35
+    assert Geom.area(7) == 7
+    assert Geom.area() == 1
+  end
+
 end
