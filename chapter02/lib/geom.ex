f9e0692a32d90ab080a3b7b954880edb878cee75
Jingjing Duan <jduan@twitter.com>
2014-09-11 13:02:18 -0700

    Etude 2-3 add docs

--- chapter02/lib/geom.ex
+++ chapter02/lib/geom.ex
@@ -1,4 +1,12 @@
 defmodule Geom do
+  @moduledoc """
+  Functions for calculating areas of geometric shapes.
+  """
+
+  @doc """
+  Calculates the area of a rectangle, given the length and width. The default
+  value for both arguments is 1.
+  """
   def area(length \\ 1, width \\ 1) do
     length * width
   end
