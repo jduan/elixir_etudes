ef04a992ed4bf185529e102d89ce0466f56ea024
Jingjing Duan <jduan@twitter.com>
2014-09-11 21:55:24 -0700

    Etude 4-4 tail recursive

--- chapter02/lib/powers.ex
+++ chapter02/lib/powers.ex
@@ -1,5 +1,5 @@
 defmodule Powers do
-  import Kernel, except: [raise: 2]
+  import Kernel, except: [raise: 2, raise: 3]
 
   def raise(_, 0), do: 1
 
@@ -8,4 +8,18 @@ defmodule Powers do
   def raise(x, n) when n > 0, do: x * raise(x, n - 1)
 
   def raise(x, n) when n < 0, do: 1.0 / raise(x, -n)
+
+  # raise2 is tail recursive
+  def raise2(_, 0), do: 1
+
+  def raise2(x, 1), do: x
+
+  def raise2(x, n) when n > 0, do: raise2(x, n, 1)
+
+  def raise2(x, n) when n < 0, do: 1.0 / raise2(x, -n)
+
+  def raise2(_x, 0, acc), do: acc
+
+  def raise2(x, n, acc), do: raise2(x, n - 1, acc * x)
+
 end
--- chapter02/test/powers_test.exs
+++ chapter02/test/powers_test.exs
@@ -8,4 +8,12 @@ defmodule PowersTest do
     assert Powers.raise(2, 0) == 1
     assert Powers.raise(2, -3) == 0.125
   end
+
+  test "raise2 x to n" do
+    assert Powers.raise2(5, 1) == 5
+    assert Powers.raise2(2, 3) == 8
+    assert Powers.raise2(1.2, 3) == 1.728
+    assert Powers.raise2(2, 0) == 1
+    assert Powers.raise2(2, -3) == 0.125
+  end
 end
