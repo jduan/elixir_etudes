c9e2baad0762fb38cfc7436ce1b13f6e6ceb8f0b
Jingjing Duan <jduan@twitter.com>
2014-09-11 21:15:44 -0700

    Etudes 3-1 and 3-2

--- chapter02/lib/geom.ex
+++ chapter02/lib/geom.ex
@@ -10,4 +10,18 @@ defmodule Geom do
   def area(length \\ 1, width \\ 1) do
     length * width
   end
+
+  def area(:rectangle, length, width) when length > 0 and width > 0 do
+    length * width
+  end
+
+  def area(:triangle, base, height) when base > 0 and height > 0 do
+    base * height / 2
+  end
+
+  def area(:ellipse, major_radius, minor_radius)
+  when major_radius > 0 and minor_radius > 0 do
+    :math.pi() * major_radius * minor_radius
+  end
+
 end
--- chapter02/test/geom_test.exs
+++ chapter02/test/geom_test.exs
@@ -12,4 +12,22 @@ defmodule GeomTest do
     assert Geom.area() == 1
   end
 
+  test "Etude 3-1 area :rectangle" do
+    assert Geom.area(:rectangle, 3, 4) == 12
+    assert Geom.area(:triangle, 3, 5) == 7.5
+    assert Geom.area(:ellipse, 2, 4) == 25.132741228718345
+  end
+
+  test "Etude 3-2 area with guards" do
+    assert_raise FunctionClauseError, fn ->
+      Geom.area(:rectangle, -3, 4)
+    end
+    assert_raise FunctionClauseError, fn ->
+      Geom.area(:triangle, -3, 4)
+    end
+    assert_raise FunctionClauseError, fn ->
+      Geom.area(:ellipse, -3, 4)
+    end
+  end
+
 end
