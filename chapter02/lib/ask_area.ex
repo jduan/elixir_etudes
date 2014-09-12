4f64f9f60b193169bb3242ab9bea1b9d00afd80f
Jingjing Duan <jduan@twitter.com>
2014-09-12 08:11:53 -0700

    Etude 5-2 use Regex.match? to validate input

--- chapter02/lib/ask_area.ex
+++ chapter02/lib/ask_area.ex
@@ -23,18 +23,35 @@ defmodule AskArea do
 
   defp do_shape(w), do: "Unknown shape #{w}"
 
+  defp do_area(_shape, :error, _b) do
+    "First input isn't a number"
+  end
+
+  defp do_area(_shape, _a, :error) do
+    "Second input isn't a number"
+  end
+
   defp do_area(shape, a, b) when a > 0 and b > 0 do
     Geom.area(shape, a, b)
   end
 
-  defp do_area(_shape, _a, _b) do
+  defp do_area(_shape, a, b) when a <= 0 or b <= 0 do
     "Both numbers must be greater than zero"
   end
 
   defp get_dimension(name1, name2) do
-    value1 = String.to_integer String.strip IO.gets("Enter #{name1} > ")
-    value2 = String.to_integer String.strip IO.gets("Enter #{name2} > ")
-    {value1, value2}
+    value1 = String.strip IO.gets("Enter #{name1} > ")
+    value2 = String.strip IO.gets("Enter #{name2} > ")
+    IO.puts inspect {str_to_number(value1), str_to_number(value2)}
+    {str_to_number(value1), str_to_number(value2)}
+  end
+
+  defp str_to_number(str) do
+    cond do
+      Regex.match?(~r/^[+-]?\d+$/, str) -> String.to_integer str
+      Regex.match?(~r/^[+-]?\d+\.\d+$/, str) -> String.to_float str
+      true -> :error
+    end
   end
 
   defp char_to_shape(str) do
