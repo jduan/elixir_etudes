b42b298b08b9b53571b3718ee31aff81767de2f9
Jingjing Duan <jduan@twitter.com>
2014-09-12 23:00:25 -0700

    Etude 6-2 Dates.julian

--- chapter02/lib/dates.ex
+++ chapter02/lib/dates.ex
@@ -1,6 +1,33 @@
 defmodule Dates do
+  @days_in_months [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  @days_in_months_leap_year [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
   def date_parts(date_str) do
     String.split(date_str, "-")
     |> Enum.map(&(String.to_integer &1))
+  end
+
+  def julian(date_str) do
+    [year, month, day] = date_parts(date_str)
+    if is_leap(year) do
+      months_list = @days_in_months_leap_year
+    else
+      months_list = @days_in_months
+    end
+
+    do_months(month - 1, months_list, 0) + day
+  end
+
+  defp is_leap(year) do
+    (rem(year,4) == 0 and rem(year,100) != 0)
+    or (rem(year, 400) == 0)
+  end
+
+  # TODO: the calculated data should be cached so future call of the same month
+  # can be fetched directly from the cache
+  defp do_months(0, days_in_months, acc), do: acc
+  defp do_months(month, days_in_months, acc) do
+    [head|tail] = days_in_months
+    do_months(month - 1, tail, acc + head)
   end
 end
--- chapter02/test/dates_test.exs
+++ chapter02/test/dates_test.exs
@@ -4,4 +4,14 @@ defmodule DatesTest do
   test "date_parts/1" do
     assert Dates.date_parts("2013-06-15") == [2013, 6, 15]
   end
+
+  test "julian" do
+    assert Dates.julian("2013-12-31") == 365
+    assert Dates.julian("2012-12-31") == 366
+    assert Dates.julian("2012-02-05") == 36
+    assert Dates.julian("2013-02-05") == 36
+    assert Dates.julian("1900-03-01") == 60
+    assert Dates.julian("2000-03-01") == 61
+    assert Dates.julian("2013-01-01") == 1
+  end
 end
